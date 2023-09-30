9d3f4a34b165590cba56caa2b14b090be55de146
Malinconico <nellix93@gmail.com>
2023-09-30 14:49:51 +0200

    refactoring task2 branch

--- task1/ecommerce_project/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import OrderList, OrderDetail, OrderItemList, OrdersSearchView
-
-urlpatterns = [
-    path('orders/', OrderList.as_view(), name='order-list'),
-    path('orders/<int:pk>/', OrderDetail.as_view(), name='order-detail'),
-    path('ordersItems/', OrderItemList.as_view(), name='order-item-list'),
-    path('orders_search/', OrdersSearchView.as_view(), name='orders_search'),
-]
